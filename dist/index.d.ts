f12a3e48d18321e95620ad09065a714f6469b798
Denis Seleznev <hcodes@yandex.ru>
2020-11-27 02:28:49 +0300

    Update index.d.ts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,3 @@
 /// <reference types="node" />
+/// <reference types="fast-average-color" />
 export declare function getAverageColor(filename: string | Buffer, options?: IFastAverageColorOptions): Promise<IFastAverageColorResult>;
