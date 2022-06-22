6f64d0b4ff34257a786776898abb73663fae5d87
Denis Seleznev <hcodes@users.noreply.github.com>
2022-06-22 16:04:47 +0300

    Remove /dist

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="node" />
-import { FastAverageColorOptions, FastAverageColorResult } from 'fast-average-color';
-export declare function getAverageColor(resource: string | Buffer, options?: FastAverageColorOptions): Promise<FastAverageColorResult>;
